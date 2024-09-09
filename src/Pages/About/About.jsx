19ac5b3ecc4e894700fcfb121559f061afcfc681
Rukshanda <rukshandanadir@gmail.com>
2024-09-09 13:50:59 -0700

    new-files

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import AboutMe from '../../Sections/AboutMe/Aboutme'
-import ProgressCircles from '../../Components/SkillsBar/SkillsBar'
+ import ProgressCircles from '../../Components/SkillsBar/SkillsBar'
 import Experience from '../../Sections/Experience/Experience'
-  
+  import AboutMe from "../../Sections/AboutMe/AboutMe"
 export default function About() {
   return (
     <div>
-        <AboutMe/>
-        <Experience/>
+      <AboutMe/>
+         <Experience/>
       <ProgressCircles/>
   
      </div>
