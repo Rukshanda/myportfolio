acfd1a3788af0aaab1bd7d24adf5ff597b162c06
Rukshanda <rukshandanadir@gmail.com>
2024-09-13 12:15:45 -0700

    new-change

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,7 @@ export default {
   theme: {
     extend: {
       boxShadow: {
-        'custom': 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
-        '3d': '0 4px 8px rgba(0, 0, 0, 0.3), 0 10px 20px rgba(0, 0, 0, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.1)',
+         '3d': '0 4px 8px rgba(0, 0, 0, 0.3), 0 10px 20px rgba(0, 0, 0, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.1)',
 
       },
     },
