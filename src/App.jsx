5a6ed29b1c431497b4ef0b0abe3d6a3c64663100
Rukshanda <rukshandanadir@gmail.com>
2024-09-15 11:23:19 -0700

    new-change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import About from "./Pages/About/About";
- 
+
+import { Helmet } from 'react-helmet';
 import Home from "./Pages/Home/Home"; // Assuming you have a Home page
 import ProjectsPage from './Pages/ProjectsPage/ProjectsPage';
 import Blogspage from './Pages/BlogsPage/Blogspage';
@@ -12,6 +13,12 @@ import ContactPage from './Pages/ContactPage/ContactPage';
 function App() {
   return (
     <Router>
+                 <Helmet>
+        <title>Portfolio-Rukshanda</title>
+        <meta name="description" content="This is Portfolio." />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="Portfolio, Web Development, Frontend Development, JavaScript , React JS  " />
+      </Helmet>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -20,10 +27,7 @@ function App() {
         <Route path="/blogs" element={<Blogspage />} />
         <Route path="/contact" element={<ContactPage />} />
 
-        {/* <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/contact" element={<Contact />} /> */}
+      
       </Routes>
       <Footer />
     </Router>
